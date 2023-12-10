934743735a8c6c41333dd84c2347c42b54472a3a
Goh Jia Ean <jjeangoh@gmail.com>
2023-12-10 18:54:41 +0800

    adding weather description

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
 
-function App() {
-  return (
+export default function App() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function handleWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=57b2c40fdae71a6ba41d72685e3226e2&units=metric`;
+    axios.get(url).then(handleWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Weather app</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search for city"
+          onChange={updateCity}
+        />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
+  if (loaded) {
+    return (
+      <div className="App">
+        {form}
+        <h3>{city}</h3>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+          <li>Description: {weather.description}</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
-
-export default App;
