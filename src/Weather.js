023a71a84e6b29ed54d549bb4216780ea0da9071
Goh Jia Ean <jjeangoh@gmail.com>
2023-12-12 22:15:19 +0800

    Updating App.css

--- src/Weather.js
+++ src/Weather.js
@@ -1,75 +1,85 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather() {
-  let [city, setCity] = useState("");
-  let [loaded, setLoaded] = useState(false);
-  let [weather, setWeather] = useState({});
+export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
+  const [weatherData, setweatherData] = useState({ ready: false });
 
   function handleWeather(response) {
-    setLoaded(true);
-    setWeather({
+    setweatherData({
+      ready: true,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
+      city: response.data.name,
     });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=57b2c40fdae71a6ba41d72685e3226e2&units=metric`;
-    axios.get(url).then(handleWeather);
+    search();
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
-  let form = (
-    <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search for city"
-              onChange={updateCity}
-              className="form-control"
-            />
-          </div>
-          <div className="col-3">
-            <button type="submit" className="btn btn-primary">
-              Search
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
-  if (loaded) {
+  function search() {
+    const apikey = "57b2c40fdae71a6ba41d72685e3226e2";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    axios.get(url).then(handleWeather);
+  }
+
+  if (weatherData.ready) {
     return (
       <div className="App">
-        {form}
-        <br></br>
-        <h3>{city}</h3>
-        <div className="row">
-          <div className="col-6">
-            <img src={weather.icon} alt={weather.description} />
-            <p>Description: {weather.description}</p>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Temperature: {Math.round(weather.temperature)}°C</li>
-              <li>Humidity: {weather.humidity}%</li>
-              <li>Wind: {weather.wind}km/h</li>
-            </ul>
+        <div className="Weathers">
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Search for city"
+                  onChange={updateCity}
+                  className="form-control"
+                />
+              </div>
+              <div className="col-3">
+                <button type="submit" className="btn btn-primary">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+          <br></br>
+          <div className="row">
+            <div className="col-6">
+              <h3>{weatherData.city}</h3>
+              <ul>
+                <li>Monday, 1 January 2023</li>
+                <li className="text-capitalize">{weatherData.description}</li>
+              </ul>
+              <img src={weatherData.icon} alt={weatherData.description} />
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Temperature: {Math.round(weatherData.temperature)}°C</li>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     );
   } else {
-    return form;
+    const apikey = "57b2c40fdae71a6ba41d72685e3226e2";
+    let city = "Kuala Lumpur";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    axios.get(url).then(handleWeather);
+
+    return "Loading";
   }
 }
