d809f3373b1960a651140e0e7f35034bda75a34e
Goh Jia Ean <jjeangoh@gmail.com>
2023-12-16 16:43:32 +0800

    Added WeatherTemperature.js for conversion

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,30 +1,28 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import Weathericon from "./Weathericon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function Weatherinfo(props) {
   return (
     <div className="Weatherinfo">
-      <div className="row mt-3">
-        <div className="col-5">
-          <h3>{props.data.city}</h3>
-          <ul className="date">
-            <li>
-              <FormattedDate date={props.data.date} />
-            </li>
-            <li className="text-capitalize">{props.data.description}</li>
-            <div className="row">
-              <div className="col-6">
-                <div className="d-flex">
-                  <Weathericon code={props.data.icon} size={52} />
-
-                  <h2>{Math.round(props.data.temperature)}°C</h2>
-                </div>
-              </div>
+      <h3 className="mt-3">{props.data.city}</h3>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex float-left">
+            <Weathericon code={props.data.icon} size={52} />
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
-          </ul>
+          </div>
         </div>
-        <div className="col-6 mt-5">
+        <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind}km/h</li>
