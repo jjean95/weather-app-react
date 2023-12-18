347e5e255b7f9585bd384c3976c5b761d797ff70
Goh Jia Ean <jjeangoh@gmail.com>
2023-12-18 22:35:17 +0800

    Forecast (one day)

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,63 +1,33 @@
-import React from "react";
-import Weathericon from "./Weathericon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  let apikey = "1a2b7258ebd456c01aef9175dfe8b709";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="ForeDay">Thu</div>
-          <Weathericon code="01d" size={30} />
-          <div className="ForeTemp">
-            <span className="ForeTemp-Max">19°</span>
-            <span className="ForeTemp-Min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="ForeDay">Thu</div>
-          <Weathericon code="01d" size={30} />
-          <div className="ForeTemp">
-            <span className="ForeTemp-Max">19°</span>
-            <span className="ForeTemp-Min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="ForeDay">Thu</div>
-          <Weathericon code="01d" size={30} />
-          <div className="ForeTemp">
-            <span className="ForeTemp-Max">19°</span>
-            <span className="ForeTemp-Min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="ForeDay">Thu</div>
-          <Weathericon code="01d" size={30} />
-          <div className="ForeTemp">
-            <span className="ForeTemp-Max">19°</span>
-            <span className="ForeTemp-Min">10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="ForeDay">Thu</div>
-          <Weathericon code="01d" size={30} />
-          <div className="ForeTemp">
-            <span className="ForeTemp-Max">19°</span>
-            <span className="ForeTemp-Min">10°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apikey = "1a2b7258ebd456c01aef9175dfe8b709";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Weathericon from "./Weathericon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="ForeDay">{day()}</div>
+      <Weathericon code={props.data.weather[0].icon} size={30} />
+      <div className="ForeTemp">
+        <span className="ForeTemp-Max">{maxTemperature()}°</span>
+        <span className="ForeTemp-Min">{minTemperature()}°</span>
+      </div>
+    </div>
+  );
+}
